e64bd5969df44f0eff3bae2f168df1bc72674a52
Yadagiri <Yadagiri@DESKTOP-FTG131U>
2022-08-16 12:22:51 +0530

    commit 2

---
+++ Clinican_Github/src/Demo1/testcase2.java
@@ -0,0 +1,11 @@
+package Demo1;
+
+public class testcase2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+			System.out.println("tEST CASE 2 By Yadagiri");
+	}
+
+}
